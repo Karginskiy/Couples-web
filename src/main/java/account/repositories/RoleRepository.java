06f584794943278fec2aeb9cabfbe45b4d41af0f
Karginskiy <hei@spark-mail.ru>
2016-12-19 17:56:04 +0300

    Created spring-orm repository for Role

---
+++ src/main/java/account/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package account.repositories;
+
+import account.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
