be401e9d48ebbf074de04aff8e46e12b4a35e1ee
Karginskiy <hei@spark-mail.ru>
2016-12-19 17:57:49 +0300

    Created UserService implementation

---
+++ src/main/java/account/services/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package account.services;
+
+import account.model.Role;
+import account.model.User;
+import account.repositories.RoleRepository;
+import account.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRoles(new HashSet<Role>(roleRepository.findAll()));
+        userRepository.save(user);
+    }
+}
